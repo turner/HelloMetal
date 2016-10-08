0365715221f44afa2fbeaf4bae8e9f9601663afa
Douglass Turner <douglass.turner@gmail.com>
2016-10-08 11:44:00 -0400

    Arcball now supports x-z plane and x-y plane orientation.

--- HelloMetal/EISArcball.swift
+++ HelloMetal/EISArcball.swift
@@ -43,14 +43,14 @@ class EISArcball {
             rotationTimer = nil;
         }
         
-        fromVector = ballLocation(screenLocation: screenLocation)
+        fromVector = ballLocationInXYPlane(screenLocation: screenLocation)
 //        fromVector.description(blurb:"from")
         
     }
 
     func updateDrag (screenLocation: CGPoint) {
 
-        toVector = ballLocation(screenLocation: screenLocation)
+        toVector = ballLocationInXYPlane(screenLocation: screenLocation)
 //        toVector.description(blurb:"  to")
 
         rotationAngle = acos(GLKVector3DotProduct(fromVector, toVector))
@@ -71,8 +71,8 @@ class EISArcball {
         let yy = CGFloat(kRotationRate * CGFloat(velocityInView.y)) + CGFloat(locationInView.y)
         let screenLocationTo = CGPoint(x:xx, y:yy)
         
-        let a = ballLocation(screenLocation:locationInView)
-        let b = ballLocation(screenLocation:screenLocationTo)
+        let a = ballLocationInXYPlane(screenLocation:locationInView)
+        let b = ballLocationInXYPlane(screenLocation:screenLocationTo)
         
         let radians = acos(GLKVector3DotProduct(a, b));
         
@@ -109,9 +109,32 @@ class EISArcball {
             rotationMatrixTouchDown = rotationMatrix
         }
     }
-    
-    func ballLocation(screenLocation:CGPoint) -> GLKVector3 {
-        
+
+    func ballLocationInXYPlane(screenLocation:CGPoint) -> GLKVector3 {
+
+        let locationInBallCoordinates = self.locationInBallCoordinates(screenLocation:screenLocation)
+
+        var ballLocation_x: CGFloat
+        ballLocation_x = (locationInBallCoordinates.x - ballCenter.x) / ballRadius;
+
+        var ballLocation_y: CGFloat
+        ballLocation_y = (locationInBallCoordinates.y - ballCenter.y) / ballRadius;
+
+        let magnitude = ballLocation_x * ballLocation_x + ballLocation_y * ballLocation_y
+
+        if (magnitude > 1.0) {
+            let scale = 1.0/sqrt(magnitude)
+            ballLocation_x *= scale;
+            ballLocation_y *= scale;
+            return GLKVector3(v:(Float(ballLocation_x), Float(ballLocation_y), Float(0)))
+        } else {
+            return GLKVector3(v:(Float(ballLocation_x), Float(ballLocation_y), Float(sqrt(1 - magnitude))))
+        }
+
+    }
+
+    func ballLocationInXZPlane(screenLocation:CGPoint) -> GLKVector3 {
+
         let locationInBallCoordinates = self.locationInBallCoordinates(screenLocation:screenLocation)
 
         var ballLocation_x: CGFloat
@@ -132,7 +155,7 @@ class EISArcball {
         }
 
     }
-    
+
     func locationInBallCoordinates(screenLocation:CGPoint) -> CGPoint {
 
         let ballBBoxSizeScreenCoordinates = max(viewBounds.width, viewBounds.height)
--- HelloMetal/Renderer.swift
+++ HelloMetal/Renderer.swift
@@ -31,10 +31,10 @@ struct Renderer {
         // viewing frustrum - eye looks along z-axis towards -z direction
         //                    +y up
         //                    +x to the right
-//        self.camera.setTransform(location:GLKVector3(v:(0, 0, 3*8)), target:GLKVector3(v:(0, 0, 0)), approximateUp:GLKVector3(v:(0, 1, 0)))
-        self.camera.setTransform(location:GLKVector3(v:(0, -2*8, 0)), target:GLKVector3(v:(0, 0, 0)), approximateUp:GLKVector3(v:(0, 0, 1)))
+        self.camera.setTransform(location:GLKVector3(v:(0, 0, 1.5*8)), target:GLKVector3(v:(0, 0, 0)), approximateUp:GLKVector3(v:(0, 1, 0)))
+//        self.camera.setTransform(location:GLKVector3(v:(0, -2*8, 0)), target:GLKVector3(v:(0, 0, 0)), approximateUp:GLKVector3(v:(0, 0, 1)))
 
-        guard let image = UIImage(named:"diagnostic") else {
+        guard let image = UIImage(named:"compass") else {
             fatalError("Error: Can not create image")
         }
         
@@ -69,7 +69,8 @@ struct Renderer {
 
     mutating func update(view: MetalView, drawableSize:CGSize) {
 
-        metallicTransform.transforms.modelMatrix = GLKMatrix4MakeXRotation(GLKMathDegreesToRadians(90)) * GLKMatrix4MakeScale(3, 2, 1)
+//        metallicTransform.transforms.modelMatrix = GLKMatrix4MakeXRotation(GLKMathDegreesToRadians(90)) * GLKMatrix4MakeScale(3, 2, 1)
+        metallicTransform.transforms.modelMatrix = GLKMatrix4MakeScale(3, 2, 1)
 
         self.camera.setProjection(fovYDegrees:Float(45), aspectRatioWidthOverHeight:Float(drawableSize.width / drawableSize.height), near: 0, far: 10)
         
