4ea7bf78b844b6a2e091a4985faa99cad8640b50
Douglass Turner <douglass.turner@gmail.com>
2016-10-24 14:08:44 -0400

    Model I/O. It begins. Again.

--- Shared/MeshModel.swift
+++ Shared/MeshModel.swift
@@ -11,46 +11,55 @@ import MetalKit
 
 struct EICube {
 
+    var mdlMesh: MDLMesh!
+    var mesh: MTKMesh!
+    var submesh: MTKSubmesh!
+    
     var vertexMetalBuffer: MTLBuffer!
     var vertexIndexMetalBuffer: MTLBuffer!
+    var indexCount: Int!
+    var indexType: MTLIndexType!
+    var primitiveType: MTLPrimitiveType!
+
+    var vertexDescriptor: MTLVertexDescriptor!
+
     var metallicTransform: MetallicTransform!
 
     init(device: MTLDevice, xExtent:Float, yExtent:Float, zExtent:Float, xTesselation:UInt32, yTesselation:UInt32, zTesselation:UInt32) {
 
         let allocator = MTKMeshBufferAllocator(device: device)
+        
+        mdlMesh = MDLMesh(boxWithExtent: vector_float3(xExtent, yExtent, zExtent),
+                              segments: vector_uint3(xTesselation, yTesselation, zTesselation),
+                              inwardNormals: false,
+                              geometryType: .triangles,
+                              allocator: allocator)
 
-        let mdlMesh = MDLMesh(boxWithExtent: vector_float3(xExtent, yExtent, zExtent),
-                segments: vector_uint3(xTesselation, yTesselation, zTesselation),
-                inwardNormals: false,
-                geometryType: .triangles,
-                allocator: allocator)
-
-        let mesh: MTKMesh!
         do {
-            mesh = try MTKMesh(mesh: mdlMesh, device: device)
+            self.mesh = try MTKMesh(mesh: self.mdlMesh, device: device)
         } catch {
             fatalError("Error: Can not create mesh")
         }
 
         self.metallicTransform = MetallicTransform(device: device)
-        
+
         // Get a reference to the first submesh of the mesh
-        let submesh = mesh.submeshes[ 0 ]
+        self.submesh = self.mesh.submeshes[ 0 ]
 
         // Extract the vertex buffer for the whole mesh
-        self.vertexMetalBuffer = mesh.vertexBuffers[ 0 ].buffer
+        self.vertexMetalBuffer = self.mesh.vertexBuffers[ 0 ].buffer
 
         // Extract the index buffer of the submesh
-        self.vertexIndexMetalBuffer = submesh.indexBuffer.buffer
+        self.vertexIndexMetalBuffer = self.submesh.indexBuffer.buffer
 
         // Get the primitive type of the mesh (triangle, triangle strip, etc.)
-        let primitiveType: MTLPrimitiveType = submesh.primitiveType
+        self.primitiveType = self.submesh.primitiveType
 
         // Get the number of indices for this submesh
-        let indexCount: Int = submesh.indexCount
+        self.indexCount = self.submesh.indexCount
 
         // Get the type of the indices (16-bit or 32-bit uints)
-        let indexType: MTLIndexType = submesh.indexType
+        self.indexType = self.submesh.indexType
 
     }
 
