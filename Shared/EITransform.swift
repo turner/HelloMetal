2de75dfe74900209cf5547b2804698ad8ebc5efc
Douglass Turner <douglass.turner@gmail.com>
2016-11-30 05:52:47 -0500

    Add normal matrix calc

--- Shared/EITransform.swift
+++ Shared/EITransform.swift
@@ -52,9 +52,32 @@ struct EITransform {
         memcpy(bufferPointer, &transform, MemoryLayout<Transform>.size)
     }
 
+    /*
+        Cheat Sheet
+
+        // PVM = P * V * M
+        GLKMatrix4 projectionViewModelTransform =
+        GLKMatrix4Multiply(camera.projectionTransform, GLKMatrix4Multiply(camera.transform, modelTransform));
+
+        // NormalTransform = Transpose( Inverse(V * M))
+        GLKMatrix3 normalTransform =
+        GLKMatrix3InvertAndTranspose(GLKMatrix4GetMatrix3(GLKMatrix4Multiply(camera.transform, modelTransform)), NULL);
+
+        // Single light source at camera. Flashlight mode.
+        GLKVector3 lightPosition = camera.location;
+
+        // transform light from world space to eye space
+        GLKVector3 lightPositionEyeSpace = GLKMatrix3MultiplyVector3(GLKMatrix4GetMatrix3(camera.transform), lightPosition);
+
+    */
+
     mutating func update (camera: EICamera, transformer:() -> GLKMatrix4) {
         transform.modelMatrix = transformer()
         transform.modelViewProjectionMatrix = camera.projectionTransform * camera.transform * transform.modelMatrix
+        
+        var success:Bool = false
+        transform.normalMatrix = GLKMatrix3InvertAndTranspose( GLKMatrix4GetMatrix3( camera.transform * transform.modelMatrix ), &success)
+//        print("is invertible \(success)" )
         update()
     }
 }
